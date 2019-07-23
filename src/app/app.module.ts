1799a36247770bdff8f3713f7f8ef76939f3c433
Mike Maharjan <Maharjan@mikes-mbp.lan>
2019-07-23 18:51:07 -0400

    adding login, register, home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { RegisterComponent } from './register';
+import { LoginComponent } from './login';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
